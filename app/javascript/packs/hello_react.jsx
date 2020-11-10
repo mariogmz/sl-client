d8c94e13b3994d9ebcfb3e2d891359ede43219ed
Mario Gomez <mariogomezmtz@gmail.com>
2020-11-10 17:54:21 -0600

    Add ESLint configuration and plugins

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,14 +1,14 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = ({name}) => (
+  <div>
+    Hello
+    {name}
+    !
+  </div>
+);
 
 Hello.defaultProps = {
   name: 'David'
