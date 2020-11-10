5b4a31c09e5dcc6e3a6feea979c7dbeb959a422c
Mario Gomez <mariogomezmtz@gmail.com>
2020-11-10 15:43:23 -0600

    Setup testing environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+
+require 'minitest/pride'
+require 'mocha/minitest'
+require 'webmock/test_unit'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -8,3 +14,5 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+Dir['./test/support/**/*'].sort.each { |f| require f }
