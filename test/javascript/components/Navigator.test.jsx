87299d2fd5d5a482bf7d6984b7181acc77b68e5b
Mario Gomez <mariogomezmtz@gmail.com>
2020-11-11 03:27:52 -0600

    Add Navigator test

---
+++ test/javascript/components/Navigator.test.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Navigator from 'components/Navigator';
+
+const navigator = {
+  listUrl: "/",
+  frequencyUrl: "/frequency",
+  duplicatesUrl: "/duplicates",
+}
+
+describe("Navigator component", () => {
+  const setup = () => (
+    render(
+      <Navigator
+        listUrl={navigator.listUrl}
+        frequencyUrl={navigator.frequencyUrl}
+        duplicatesUrl={navigator.duplicatesUrl}
+      />
+    )
+  );
+
+  test("Shows Fabs for each link", () => {
+    const { getByText } = setup();
+
+    expect(getByText("List")).toBeInTheDocument();
+    expect(getByText("Frequency")).toBeInTheDocument();
+    expect(getByText("Duplicates")).toBeInTheDocument();
+  })
+});
