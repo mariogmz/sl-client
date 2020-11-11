6b9a966faaf6bbb84b4b89fd846ca499290d53a2
Mario Gomez <mariogomezmtz@gmail.com>
2020-11-10 22:11:52 -0600

    Add react_rails with hello world component

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -3,7 +3,8 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#index" do
+    get :index
+    assert_response :success
+  end
 end
